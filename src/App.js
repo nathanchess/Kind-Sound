2a276c1e0bf2012421da927886d7f43636cdf0a8
Nathan Che <nanache21@gmail.com>
2023-01-07 22:01:49 -0800

    react convert

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import TopBar from './Components/TopBar';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;

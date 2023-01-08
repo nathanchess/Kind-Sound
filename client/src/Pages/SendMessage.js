6f5d369e7459871dbc464eac517e03f151a6ee3e
Andy Khau <fakeemail1441@gmail.com>
2023-01-08 04:39:12 -0800

    Add SendMesage css

--- client/src/Pages/SendMessage.js
+++ client/src/Pages/SendMessage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './SendMessage.css'
+
+// If you want to you can move all the css in SendMessage to App.css
 
 const SendMessage = () => {
   return (
-    <div>
-        send messagee page
+    <div className="send-message">
+        <textarea type="text" id="message-input" placeholder="Ex: I hope you have a wonderful day! (max 200 characters)" maxLength="200">
+
+        </textarea>
     </div>
   )
 }

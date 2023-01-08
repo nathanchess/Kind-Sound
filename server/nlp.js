33714f7bdae36025a5ae3ec070467443524ca952
Andy Khau <fakeemail1441@gmail.com>
2023-01-08 03:43:19 -0800

    Create sentiment analysis

---
+++ server/nlp.js
@@ -0,0 +1,22 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+
+const analyzeText = async (message, apiKey) => {
+    /* returns true is the message is positive and false is the message is negative */
+    const configuration = new Configuration({
+        apiKey: apiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+        model: "text-curie-001",
+        prompt: `Decide if the sentiment of this message is positive, neutral, or negative:${message}`,
+        temperature: 0,
+        max_tokens: 100,
+    });
+
+    const text = response.data.choices[0].text;
+    return text.includes('positive');
+}
+
+export default analyzeText;
+
